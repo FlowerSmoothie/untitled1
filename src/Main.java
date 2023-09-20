5b4655a48258c9de213e49af7f8126125108041a
Stepan4kProgger <istepan2004@gmail.com>
2023-09-21 00:52:31 +0300

    Some changes

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,32 @@ import Interfaces.*;
 
 
 public class Main {
+
+    private static void helloMsg(){
+        System.out.println("The best lab")
+    }
+
     public static void main(String[] args) {
+        helloMsg();
 
-       Weapon obj1 = new FlyingKnife();
-       System.out.println(obj1.giveDamageToEnemy(10,15));
+        Weapon obj1 = new FlyingKnife();
+        System.out.println(obj1.giveDamageToEnemy(10, 15));
 
-       MagicWeapon obj2 = new MagicBook();
-       System.out.println(obj2.giveMagicDamage(90,80));
-       System.out.println(obj2.giveDamageToEnemy(13,12));
+        MagicWeapon obj2 = new MagicBook();
+        System.out.println(obj2.giveMagicDamage(90, 80));
+        System.out.println(obj2.giveDamageToEnemy(13, 12));
 
-       MagicBook obj3 = new MagicBook();
-       System.out.println(obj3.giveDamageToEnemy(10,19));
-       System.out.println(obj3.giveMagicDamage(10 ,19));
+        MagicBook obj3 = new MagicBook();
+        System.out.println(obj3.giveDamageToEnemy(10, 19));
+        System.out.println(obj3.giveMagicDamage(10, 19));
 
-       MagicWand obj4 = new RainbowRod();
-       System.out.println(obj4.giveDamageToEnemy(10,19));
+        MagicWand obj4 = new RainbowRod();
+        System.out.println(obj4.giveDamageToEnemy(10, 19));
 
-       MagicWand obj5 = new VenomStaff();
-       System.out.println(obj5.giveDamageToEnemy(10,19));
+        MagicWand obj5 = new VenomStaff();
+        System.out.println(obj5.giveDamageToEnemy(10, 19));
 
     }
+
+
 }
